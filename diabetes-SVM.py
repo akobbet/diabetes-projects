f3a1418926ffb35b6c41e37ae9a6c993fe75e3c4
akobbet <akobbet@yahoo.fr>
2018-12-25 14:09:17 +0000

    Create diabetes-SVM.py
    file save as py

---
+++ diabetes-SVM.py
@@ -0,0 +1,362 @@
+
+# coding: utf-8
+
+# # Import Libraries
+
+# In[6]:
+
+
+# Lets import all the modules, functions & objects for this project
+
+
+# In[5]:
+
+
+# %load binary_class_libraries
+import numpy
+from numpy import set_printoptions
+from matplotlib import pyplot
+import pandas as pd
+from pandas import read_csv
+from pandas import set_option
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Binarizer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+
+# # Load Dataset
+# 
+
+# In[7]:
+
+
+# I am going to load the dataset from my computer. The dataset is from UCI Machine Learning repo.
+
+
+# In[8]:
+
+
+filename = 'diabetes.csv'
+
+
+# In[11]:
+
+
+# read diabetes.csv into a DataFrame called filename
+df = pd.read_csv(filename)
+
+
+# # Summary of the Dataset
+
+# In[12]:
+
+
+# shape of the  DataFrame
+df.shape
+
+
+# In[21]:
+
+
+# types
+df.dtypes
+
+
+# In[13]:
+
+
+# view the first 5 rows of  the dataset
+df.head()
+
+
+# In[14]:
+
+
+# Statistical summary 
+df.describe()
+
+
+# In[20]:
+
+
+#Class distribution
+df.groupby('class').size()
+
+
+# # Data Visualization
+
+# In[22]:
+
+
+# histograms plots
+df.hist(sharex=False, sharey=False, xlabelsize=1, ylabelsize=1)
+pyplot.show()
+
+
+# In[26]:
+
+
+# density plots
+df.plot(kind='density', subplots=True, layout=(8,8), sharex=False, legend=False, fontsize=1)
+pyplot.show()
+
+
+# In[27]:
+
+
+# correlation matrix
+# plot of the correlations between attributes from the dataset
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(df.corr(), vmin=-1, vmax=1, interpolation='none')
+fig.colorbar(cax)
+pyplot.show()
+
+
+# # Evaluate Algorithms: Baseline
+
+# In[29]:
+
+
+# Split-out validation dataset
+array = df.values
+X= array[:,0:8]
+Y= array[:,8]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X,Y, test_size=validation_size, random_state=seed)
+
+
+# In[30]:
+
+
+# test options and evaluation metric
+num_folds=10
+seed = 7
+scoring ='accuracy'
+
+
+# In[31]:
+
+
+# Spot-Check algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+
+# In[34]:
+
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+# In[35]:
+
+
+# Visualization of the Distribution of Algorithms Performance
+# compare Algorithms
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+
+# In[36]:
+
+
+# Evaluate the same algorithms on differents views of the dataset (normalized, standardized,..)
+
+
+# In[37]:
+
+
+# standardize the dataset
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR',
+LogisticRegression())])))
+pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()),('LDA',
+LinearDiscriminantAnalysis())])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN',
+KNeighborsClassifier())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART',
+DecisionTreeClassifier())])))
+pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()),('NB',
+GaussianNB())])))
+pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()),('SVM', SVC())])))
+
+
+# In[39]:
+
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+# In[40]:
+
+
+# Visualization of the Distribution of Algorithms Performance
+# compare Algorithms
+fig = pyplot.figure()
+fig.suptitle('Scaled Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+
+# In[41]:
+
+
+# Algorithm Tuning
+
+
+# In[43]:
+
+
+# Tuning KNN (tuning scaled KNN)
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+neighbors = [1,3,5,7,9,11,13,15,17,19,21]
+param_grid = dict(n_neighbors=neighbors)
+model = KNeighborsClassifier()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# In[44]:
+
+
+# Tuning SVM (tuning scaled SVM)
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+c_values = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 1.3, 1.5, 1.7, 2.0]
+kernel_values = ['linear', 'poly', 'rbf', 'sigmoid']
+param_grid = dict(C=c_values, kernel=kernel_values)
+model = SVC()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# # Evaluate Ensemble Algorithms
+
+# In[45]:
+
+
+# ensembles
+# Spot-Check algorithms
+ensembles = []
+ensembles.append(('AB', AdaBoostClassifier()))
+ensembles.append(('GBM', GradientBoostingClassifier()))
+ensembles.append(('RF', RandomForestClassifier()))
+ensembles.append(('ET', ExtraTreesClassifier()))
+
+
+# In[47]:
+
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in ensembles:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+# In[48]:
+
+
+# Visualization of the Distribution of Algorithms Performance
+# compare Algorithms
+fig = pyplot.figure()
+fig.suptitle('Ensemble Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+
+# # Finalize Model
+
+# In[49]:
+
+
+# prepare the model
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+model = SVC(C=1.5)
+model.fit(rescaledX, Y_train)
+
+
+# In[50]:
+
+
+# estimate the accauracy on validation dataset
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
